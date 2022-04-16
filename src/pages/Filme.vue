3bb39dab95e20a8159417310a9a1732477e07f2e
castrintt <igordc38@gmail.com>
2022-04-16 19:08:18 -0300

    Projeto finalizado

--- src/pages/Filme.vue
+++ src/pages/Filme.vue
@@ -1,15 +1,126 @@
 <template>
   <div id="filme">
-      
+    <div class="loading" v-show="this.aux">
+      <Loading />
+    </div>
+    <div class="container">
+      <h2>
+        {{ filmes.nome }}
+      </h2>
+      <img :src="filmes.foto" :alt="filmes.nome" />
+      <h3>Sinopse</h3>
+      <p>
+        {{ filmes.sinopse }}
+      </p>
+      <div class="botoes">
+        <button>
+          <router-link to="/">Home</router-link>
+        </button>
+        <button @click="salvarFilme">
+          Salvar
+        </button>
+        <button>
+          <a :href="`https://youtube.com/results?search_query=${filmes.nome}`" target="_blank">
+            Trailer
+          </a>
+          </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Loading from "@/components/Loading.vue";
+import api from "../services/api.js";
 export default {
-    name:'Filme'
-}
+  name: "Filme",
+  data() {
+    return {
+      filmes: [],
+      aux: true,
+      salvos: []
+    };
+  },
+  props: ["id"],
+  async created() {
+    const response = await api.get(`?api=filmes/${this.id}`);
+
+    this.filmes = response.data;
+    this.aux = false;
+  },
+  components: {
+    Loading,
+  },
+  methods:{
+    salvarFilme(){
+      const minhaLista = localStorage.getItem('myFilme')
+      let filme = JSON.parse(minhaLista) || []
+      
+      const hasFilme = filme.some((filme) => filme.id === this.filmes.id)
+      if(hasFilme){
+        alert('Você já tem esse filme salvo!')
+        return 
+      }
+
+      filme.push(this.filmes)
+      localStorage.setItem('myFilme',JSON.stringify(filme))
+      alert('Filme salvo com sucesso')
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.loading {
+  margin: 0 auto;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 100%;
+  width: 70%;
+  margin: 0 auto;
+}
 
+h2 {
+  color: #fff;
+  margin-bottom: 0px;
+  background: brown;
+  padding: 15px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+}
+
+.container img {
+  width: 100%;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+}
+
+button {
+  margin-right: 5px;
+  border-radius: 5px;
+  margin-top: 15px;
+  cursor: pointer;
+  border: 0;
+  padding: 12px;
+  font-size: 20px;
+  transition: all 0.5s;
+  outline: none;
+}
+button:hover {
+  background: brown;
+  color: #fff;
+}
+
+a {
+  text-decoration: none;
+  color: #000;
+  transition: all 0.5s;
+}
+
+a:hover {
+  color: #fff;
+}
 </style>
--- src/pages/MeusFilmes.vue
+++ src/pages/MeusFilmes.vue
@@ -1,15 +1,100 @@
 <template>
   <div id="meus-filmes">
-      <h1>Meus filmes</h1>
+    <h1>Meus filmes</h1>
+    <span v-if="this.filmes.length <= 0">
+      Você não possui nenhum filme savo :(
+    </span>
+    <ul>
+      <li v-for="filme in filmes" :key="filme.id">
+        <span>{{ filme.nome }}</span>
+        <div>
+          <button>
+            <router-link :to="`/filme/${filme.id}`">Ver Detalhes</router-link>
+          </button>
+          <button @click="excluir(filme.id)">Excluir</button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-    name:'MeusFilmes'
-}
+  name: "MeusFilmes",
+  data() {
+    return {
+      filmes: [],
+    };
+  },
+  created() {
+    const minhaLista = localStorage.getItem("myFilme");
+    this.filmes = JSON.parse(minhaLista) || [];
+   
+  },
+  methods:{
+    excluir(id){
+      let filtroFilmes = this.filmes.filter((filme) => {
+        return filme.id != id
+      })
+      return this.filmes = filtroFilmes
+    }
+  },
+  watch:{
+    filmes(){
+      localStorage.setItem('myFilme',JSON.stringify(this.filmes))
+    }
+  }
+};
 </script>
 
-<style>
+<style escoped>
+#meus-filmes {
+  display: flex;
+  flex-direction: column;
+  max-width: 80%;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+}
 
+ul {
+  padding: 0;
+  width: 100%;
+}
+
+ul li {
+  padding: 0;
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 10px auto;
+  width: 100%;
+
+}
+
+span {
+  font-size: 25px;
+}
+
+button {
+  margin-left: 10px;
+  padding: 5px;
+  border-radius: 10px;
+  outline: none;
+  border: none;
+  background-color: rgba(128, 128, 128, 0.712);
+  cursor: pointer;
+  transition: 0.5s;
+}
+button a {
+  text-decoration: none;
+  color: black;
+}
+button:hover {
+  transition-duration: 0.5s;
+  transition-timing-function: ease-in-out;
+  background-color: brown;
+}
 </style>
